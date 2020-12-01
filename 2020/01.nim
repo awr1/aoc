685e0248f36ee009cc068d2b805e1d767e2216ff
awr1 <41453959+awr1@users.noreply.github.com>
2020-12-01 02:54:00 -0600

    Day 01/2020: 'Report Repair' using template recursion

---
+++ 2020/01.nim
@@ -0,0 +1,17 @@
+import strutils, sequtils
+
+func multiply(expenses :openArray[int]; degrees :static[int]) :int =
+  var tally :array[degrees, int]
+  template loop(input :openArray[int]; level :static[int]) =
+    for idx, ele in input:
+      tally[level] = ele
+      when level == 0:
+        if tally.foldl(a + b) == 2020: return tally.foldl(a * b)
+      else:
+        input[idx .. input.high].loop(level - 1)
+  expenses.loop(degrees - 1)
+
+var (line, expenses) = ("", newSeq[int]())
+while stdin.readLine(line) and line != "": expenses &= line.parseInt
+echo expenses.multiply(degrees = 2)
+echo expenses.multiply(degrees = 3)
