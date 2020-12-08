3866cb6fd580a5dd7d256d9cd8f41887f96d61c7
awr1 <41453959+awr1@users.noreply.github.com>
2020-12-08 10:38:48 -0600

    Day 08/2020: Reimplemented challenge using stack backtracking

--- 2020/day_08.nim
+++ 2020/day_08.nim
@@ -1,36 +1,28 @@
 import strutils, strscans, sequtils
 
-type Kind = enum acc, jmp, nop
+type Kind = enum acc, nop, jmp
 
 var ops = stdin.readAll.splitLines.filterIt(not it.isEmptyOrWhitespace).mapIt(
   (let x = it.split; (kind : parseEnum[Kind](x[0]), arg : parseInt(x[1]))))
 
-proc run() :auto =
-  var
-    exec = newSeq[bool](ops.len)
-    regs = (acc : 0, pc : 0)
-  while regs.pc in ops.low .. ops.high:
-    if exec[regs.pc]:
-      return (terminates : false, acc : regs.acc)
-    exec[regs.pc] = true
-    case ops[regs.pc].kind
-    of acc:
-      regs.acc += ops[regs.pc].arg
-      regs.pc  += 1
-    of jmp:
-      regs.pc += ops[regs.pc].arg
-    of nop:
-      regs.pc += 1
-  return (terminates : true, acc : regs.acc)
+proc solve() :tuple[part1, part2 :int] =
+  var (stack, terminates) = (newSeq[int](), false)
+  proc switch(x :int) =
+    if   ops[x].kind == jmp: ops[x].kind = nop
+    elif ops[x].kind == nop: ops[x].kind = jmp
+  proc run(pushing = true) :int =
+    var (pc, exec) = (0, newSeq[bool](ops.len))
+    while pc in ops.low .. ops.high:
+      if exec[pc]: return
+      exec[pc] = true
+      case ops[pc].kind
+      of acc: result += ops[pc].arg;     pc += 1
+      of nop: (if pushing: stack &= pc); pc += 1
+      of jmp: (if pushing: stack &= pc); pc += ops[pc].arg
+    terminates = true
+  result.part1 = run()
+  for i in stack:
+    i.switch; defer: i.switch # preserve code on fail
+    if (let x = run(pushing = false); terminates): result.part2 = x; return
 
-echo run().acc
-for idx, op in ops.mpairs:
-  proc switch() :bool =
-    if   op.kind == nop: op.kind = jmp; return true
-    elif op.kind == jmp: op.kind = nop; return true
-
-  if switch():
-    if (let pass = run(); pass.terminates):
-      echo pass.acc; break
-    else:
-      discard switch()
+echo solve()
