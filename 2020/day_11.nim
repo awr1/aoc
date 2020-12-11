6885bc7793d16f7b7d6888c0bd9e83b89d7f7f7c
awr1 <41453959+awr1@users.noreply.github.com>
2020-12-11 02:34:49 -0600

    Day 11/2020: Cleaned up and generalized solution by using line-of-sight code for part 1

--- 2020/day_11.nim
+++ 2020/day_11.nim
@@ -1,4 +1,4 @@
-import strutils, sequtils, sugar, os
+import strutils, sequtils, sugar
 
 type Tile = enum Floor, Empty, Occupied
 
@@ -14,29 +14,21 @@ proc simulate(input :seq[seq[Tile]], visibility :bool) :seq[seq[Tile]] =
         var adjacents = 0
         for dY in [-1, 0, +1]:
           for dX in [-1, 0, +1]:
-            proc bounded(xx, yy :int) :bool =
-              xx in row.low .. row.high and yy in input.low .. input.high
-
             if (dX, dY) != (0, 0):
-              if visibility:
-                var i = 1
-                while (let (xx, yy) = (x + (dX * i), y + (dY * i)); bounded(xx, yy)):
-                  if input[yy][xx] == Empty:      break
-                  elif input[yy][xx] == Occupied: adjacents += 1; break
-                  i += 1
-              else:
-                let (xx, yy) = (x + dX, y + dY)
-                if bounded(xx, yy) and input[yy][xx] == Occupied: adjacents += 1
-
-        if (adjacents >= (if visibility: 5 else: 4)) and input[y][x] == Occupied:
+              var i = 1
+              while (let (xx, yy) = (x + (dX * i), y + (dY * i));
+                     xx in row.low .. row.high and yy in input.low .. input.high):
+                if   input[yy][xx] == Occupied: adjacents += 1; break
+                elif input[yy][xx] == Empty or not visibility:  break
+                i += 1
+        if adjacents >= (if visibility: 5 else: 4) and tile == Occupied:
           result[y][x] = Empty
-        elif adjacents == 0 and input[y][x] == Empty:
+        elif adjacents == 0 and tile == Empty:
           result[y][x] = Occupied
 
 proc equilibrium(visibility :bool) :int =
   var (a, b) = (rows, rows.simulate(visibility))
-  while a != b:
-    a = b.simulate(visibility); swap(a, b)
+  while a != b: a = b.simulate(visibility); swap(a, b)
   a.mapIt(it.count(Occupied)).foldl(a + b)
 
 echo equilibrium(visibility = false)
