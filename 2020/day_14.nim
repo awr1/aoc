2a19fdff168b24be3a0c28d9c4577e101f945c0d
awr1 <41453959+awr1@users.noreply.github.com>
2020-12-14 01:24:02 -0600

    Day 14/2020: Performed cleanup, variable renaming, removed debug echo

--- 2020/day_14.nim
+++ 2020/day_14.nim
@@ -8,29 +8,25 @@ var
 while stdin.readLine(line) and line != "":
   var memAddr, memValue = 0
   if line.startsWith("mask = "):
-    echo(line)
-    line.removePrefix("mask = ")
-    mask.reset
+    line.removePrefix("mask = "); mask.reset
     for idx, ele in line:
-      let bitidx = 35 - idx
-      if   ele == 'X': mask.x.setBit(bitidx)
-      elif ele == '1': mask.overwrite.setBit(bitidx)
-      elif ele == '0': mask.overwrite.clearBit(bitidx)
+      let bit = 35 - idx
+      if   ele == 'X': mask.x.setBit(bit)
+      elif ele == '1': mask.overwrite.setBit(bit)
+      elif ele == '0': mask.overwrite.clearBit(bit)
   elif line.scanf("mem[$i] = $i", memAddr, memValue):
     mem1[memAddr] = bitor(bitand(memValue,       mask.x),
                           bitand(mask.overwrite, bitnot(mask.x)))
     func nthSetBit(x, n :int) :int =
-      var xx = x
-      for i in 0 ..< n:
-        result += xx.firstSetBit
-        xx      = xx shr xx.firstSetBit
+      var xx = x # params are constant in Nim, so a new variable is needed
+      for i in 0 ..< n: result += xx.firstSetBit; xx = xx shr xx.firstSetBit
     let pc = 0 ..< popcount(mask.x)
-    for i in 0 .. toMask[int64](pc):
+    for counter in 0 .. toMask[int64](pc):
       var adjustedAddr = memAddr
-      for j in pc:
-        let n = mask.x.nthSetBit(j + 1) - 1
-        if i.testBit(j): adjustedAddr.setBit(n)
-        else:            adjustedAddr.clearBit(n)
+      for maskBit in pc:
+        let addrBit = mask.x.nthSetBit(maskBit + 1) - 1
+        if counter.testBit(maskBit): adjustedAddr.setBit(addrBit)
+        else:                        adjustedAddr.clearBit(addrBit)
       mem2[bitOr(mask.overwrite, adjustedAddr)] = memValue
 
 echo toSeq(mem1.values).foldl(a + b)
