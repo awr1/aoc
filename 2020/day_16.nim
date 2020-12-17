c0d430671ca055a57a78abc02eb731554ff8c22c
awr1 <41453959+awr1@users.noreply.github.com>
2020-12-16 23:12:20 -0600

    Day 16/2020: Use collect to gather up sequences/sets

--- 2020/day_16.nim
+++ 2020/day_16.nim
@@ -1,9 +1,10 @@
-import strutils, strscans, sequtils, intsets, sugar
+import strutils, strscans, sequtils, sets, math, sugar
 
 var
   line            = ""
   slices          = newSeq[tuple[name :string; ranges :array[2, Slice[int]]]]()
   (your, nearby)  = (newSeq[int](), newSeq[seq[int]]())
+
 while stdin.readLine(line):
   var (name, pair) = ("", [0 .. 0, 0 .. 0])
   if line.scanf("$+: $i-$i or $i-$i",
@@ -16,26 +17,20 @@ while stdin.readLine(line):
 
 func validates(slice :slices[0].type; x :int) :bool =
   x in slice.ranges[0] or x in slice.ranges[1]
-var error = 0
-for ticket in nearby:
-  for entry in ticket:
-    if not slices.anyIt(it.validates(entry)): error += entry
 
-let valids    = nearby.filter(x => x.all(y => slices.anyIt(it.validates(y))))
-var sliceSets = newSeq[IntSet]()
-for yourIdx, _ in your:
-  let nums     = valids.mapIt(it[yourIdx])
-  var sliceSet = initIntSet() # wish I could use collect() here...
-  for sliceIdx, slice in slices:
-    if nums.allIt(slice.validates(it)): sliceSet.incl(sliceIdx)
-  sliceSets &= sliceSet
+var sliceSets = collect newSeq:
+  for yourIdx, _ in your:
+    let nums = nearby.filter(x => x.all(y => slices.anyIt(it.validates(y))))
+      .mapIt(it[yourIdx])
+    collect initHashSet:
+      for sliceIdx, slice in slices:
+        if nums.allIt(slice.validates(it)): {sliceIdx}
 
 for idxA, a in sliceSets.mpairs:
   for idxB, b in sliceSets.pairs:
     if idxA != idxB and (let c = a - b; c.card > 0): a = c
 
-echo error
-echo zip(your, sliceSets.mapIt(it.toSeq[0]).mapIt(slices[it]))
+echo nearby.map(x => x.filter(y => not slices.anyIt(it.validates(y))).sum).sum
+echo zip(your, sliceSets.mapIt(slices[it.toSeq[0]]))
   .filterIt(it[1].name.startsWith("departure"))
-  .mapIt(it[0])
-  .foldl(a * b)
+  .mapIt(it[0]).prod
